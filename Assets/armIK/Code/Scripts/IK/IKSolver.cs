c73d2ec0fcbfbb1344179a3c47a69e6290374cdd
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2023-05-12 03:10:50 +0200

    add second neighbor fabrikr constraining

--- Assets/armIK/Code/Scripts/IK/IKSolver.cs
+++ Assets/armIK/Code/Scripts/IK/IKSolver.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,44 +8,33 @@ public class IKSolver : MonoBehaviour {
     [SerializeField] private int maxSteps = 16;
     [SerializeField] private float targetDistanceTolerance = 0.001f;
     [SerializeField] private float minIterationImprovement = 0.005f;
-    [SerializeField] private bool solve = false;
 
     private List<IKJoint> joints = new List<IKJoint>();
     private List<float> distances = new List<float>();
     private List<Quaternion> relativeRotations = new List<Quaternion>();
     private float distanceSum = 0f;
 
-    Vector3 origin = Vector3.zero;
-    Quaternion originRotation = Quaternion.identity;
     Transform target = null;
 
     private void Awake() {
         InitializeSystem();
     }
 
-    private void Update() {
-        //if (!solve)
-        //     return;
-
-        //solve = false;
-
+    private void FixedUpdate() {
         if (joints.Count == 0)
             return;
 
-        origin = joints[0].transform.position;
-        originRotation = joints[0].transform.rotation;
         target = (joints[joints.Count - 1] as IKEffector)?.target;
         if (!target)
             return;
 
-        if (Vector3.Distance(origin, target.position) >= distanceSum) {
+        if (Vector3.Distance(joints[0].position, target.position) >= distanceSum) {
             FABRIKPass();
             return;
         }
 
-
         var targetDistance = DistanceToTarget();
-        for (int i = 0; true || (i < maxSteps && targetDistance > targetDistanceTolerance); i++) {
+        for (int i = 0; i < maxSteps && targetDistance > targetDistanceTolerance; i++) {
             FABRIKPass();
 
             var newTargetDistance = DistanceToTarget();
@@ -79,58 +69,66 @@ public class IKSolver : MonoBehaviour {
 
     private void FABRIKPass() {
         // Forward reaching
-        var PPrevOriginalPosition = joints[joints.Count - 1].position;
         joints[joints.Count - 1].position = target.position;
+        // Root joint is skipped so we dont have to reset its position
         for (int i = joints.Count - 2; i > 0; i--) {
             var PPrev = joints[i + 1];
             var PI = joints[i];
             var PNext = joints[i - 1];
 
-            // project PI into the constrained plane of PPrev
-            var projectedPosition = Vector3.ProjectOnPlane(PI.position - PPrev.position, PPrev.transform.up);
-            PPrevOriginalPosition = PI.position;
-            PI.position = projectedPosition.normalized * distances[i] + PPrev.position;
+            var PIHat = findPIHat(PPrev, PI, distances[i], relativeRotations[i]);
+            PI.position = PIHat.Item1;
+            PI.rotation = PIHat.Item2;
 
-            PI.rotation = Quaternion.LookRotation((PPrev.position - PI.position).normalized, PPrev.transform.up) * relativeRotations[i];
-
-            var phiIN = Vector3.Cross(PNext.transform.up, (PNext.position - PPrev.position).normalized).normalized;
-            //Debug.DrawRay(PPrev.position + (PNext.position - PPrev.position) / 2, phiIN, Color.red, 5f);
-
-            /*
-            var jointA = joints[i + 1];
-            var jointB = joints[i];
-
-            var distance = distances[i];
-            var difference = (jointA.transform.position - jointB.transform.position).magnitude;
-            var lambda = distance / difference;
-            jointB.transform.position = (1 - lambda) * jointA.transform.position + lambda * jointB.transform.position;
-            */
+            var theta = findTheta(PPrev, PI, PNext);
+            PI.transform.RotateAround(PPrev.position, PPrev.transform.up, theta);
         }
 
         // Backward reaching
-        //joints[0].transform.position = origin;
-        //joints[0].transform.rotation = originRotation;
         for (int i = 1; i < joints.Count; i++) {
-            //var PNext = joints[i + 1];
             var PI = joints[i];
             var PPrev = joints[i - 1];
 
-            // project PI into the constrained plane of PPrev
-            var projectedPosition = Vector3.ProjectOnPlane(PI.position - PPrev.position, PPrev.transform.up);
-            PI.position = projectedPosition.normalized * distances[i - 1] + PPrev.position;
+            var PIHat = findPIHat(PPrev, PI, distances[i - 1], relativeRotations[i - 1]);
+            PI.position = PIHat.Item1;
+            PI.rotation = PIHat.Item2;
 
-            PI.rotation = Quaternion.LookRotation((PPrev.position - PI.position).normalized, PPrev.transform.up) * relativeRotations[i - 1];
+            if (i == joints.Count - 1)
+                continue;
 
-            /*
-            var jointA = joints[i];
-            var jointB = joints[i + 1];
-
-            var distance = distances[i];
-            var difference = (jointA.transform.position - jointB.transform.position).magnitude;
-            var lambda = distance / difference;
-            jointB.transform.position = (1 - lambda) * jointA.transform.position + lambda * jointB.transform.position;
-            */
+            var PNext = joints[i + 1];
+            var theta = findTheta(PPrev, PI, PNext);
+            PI.transform.RotateAround(PPrev.position, PPrev.transform.up, theta);
         }
+    }
+
+    /// <summary>
+    ///     Find a new P_i that satisfies the P_prev constraint
+    /// </summary>
+    private Tuple<Vector3, Quaternion> findPIHat(IKJoint PPrev, IKJoint PI, float distance, Quaternion relativeRotation) {
+        var projectedPosition = Vector3.ProjectOnPlane(PI.position - PPrev.position, PPrev.transform.up);
+        var position = projectedPosition.normalized * distance + PPrev.position;
+        var rotation = Quaternion.LookRotation((PPrev.position - position).normalized, PPrev.transform.up) * relativeRotation;
+
+        return new Tuple<Vector3, Quaternion>(position, rotation);
+    }
+
+    /// <summary>
+    ///     Calculate the angle to rotate P_i around the actuation vector of P_prev to satisfy the P_next constraint,
+    ///     while not breaking the P_prev constraint
+    /// </summary>
+    private float findTheta(IKJoint PPrev, IKJoint PI, IKJoint PNext) {
+        var phiIN = Vector3.Cross(PNext.transform.up, (PNext.position - PPrev.position).normalized).normalized;
+        // Debug.DrawRay(PPrev.position + (PNext.position - PPrev.position) / 2, phiIN, Color.red, 5f);
+
+        var phiINProjectedToPhiPrev = Vector3.ProjectOnPlane(phiIN, PPrev.transform.up);
+        if (Vector3.Dot(phiINProjectedToPhiPrev, PI.transform.up) < 0)
+            phiINProjectedToPhiPrev *= -1f;
+
+        // Debug.DrawRay(PI.position, phiINProjectedToPhiPrev, Color.magenta, 5f);
+
+        Quaternion.FromToRotation(PI.transform.up, phiINProjectedToPhiPrev).ToAngleAxis(out var angle, out var axis);
+        return angle;
     }
 
     private float DistanceToTarget() {
